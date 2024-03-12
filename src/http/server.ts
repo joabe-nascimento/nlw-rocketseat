aeb47fff94c0a5f74a2d9c9c047be9ffb8e4f54d
Joabe <joabefnascimento1@outlook.com>
2024-03-12 01:16:33 -0300

    etapas da requisição post

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,9 +2,14 @@ import fastify from "fastify"
 
 const app = fastify()
 
-app.get('/hello', () => {
+//GET, POST, PUT, DELETE
+app.post('/polls', (request) => {
+    console.log(request.body)
+
     return 'Hello Joabe'
 })
+
+
 
 app.listen({port: 3333}).then(() => {
     console.log("HTTP server running")
